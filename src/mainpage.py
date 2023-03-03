5a20441a66af8dd7bdcd308cfa09337931090cc6
egversman <107862201+egversman@users.noreply.github.com>
2023-03-02 22:35:18 -0800

    Initial commit

---
+++ src/mainpage.py
@@ -0,0 +1,18 @@
+"""! @file mainpage.py
+"""
+
+"""! @section sec_software Software Design
+    The design is documented in the ensuing subsections.
+"""
+
+"""! @subsection subsec_task1 Task One
+    Description of task one.
+"""
+
+"""! @subsection subsec_task2 Task Two
+    Description of task one.
+ """
+
+"""! @subsection subsec_task3 Task Three
+    Description of task one.
+"""
